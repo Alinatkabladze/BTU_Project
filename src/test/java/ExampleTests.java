7e27e4bd20310642a572fb3d443c50042964915d
Salome Tkhilaishvili <Salome.Tkhilaishvili@IntegrationPoint.com>
2022-09-29 13:21:26 +0400

    fist homework

--- src/test/java/ExampleTests.java
+++ src/test/java/ExampleTests.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
+import org.openqa.selenium.support.ui.Select;
 public class ExampleTests {
 
     @Test
@@ -17,7 +17,19 @@ public class ExampleTests {
         WebElement button=driver.findElement(By.xpath("//ul/li/a"));
         button.click();
         WebElement text = driver.findElement(By.xpath("//*[@id=\"content\"]/div/h3"));
-        Assert.assertEquals(text.getText(),"No Test");
+        Assert.assertEquals(text.getText(),"A/B Test Control");
+
+        driver.navigate().refresh();
+        driver.navigate().to("http://the-internet.herokuapp.com/");
+        WebElement dropdown=driver.findElement(By.xpath("//*[@id=\"content\"]/ul/li[11]/a"));
+        dropdown.click();
+
+        //WebElement dropdownComponentText = driver.findElement(By.xpath(("//*[@id=\"dropdown\"]")));
+
+        WebElement selectedOptionText= driver.findElement(By.xpath(("//*[@id=\"dropdown\"]/option[2]")));
+        Select select = new Select(driver.findElement(By.id("dropdown")));
+        select.selectByVisibleText(selectedOptionText.getText());
+        select.selectByIndex(1);
 
 
 
