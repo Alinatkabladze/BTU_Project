df7e80047c7c8be8e4d8657620d0591bee67c1f2
Sopio Berishvili <85607071+Sophula@users.noreply.github.com>
2022-10-03 15:47:28 +0400

    Update ExampleTests.java

--- src/test/java/ExampleTests.java
+++ src/test/java/ExampleTests.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.openqa.selenium.support.ui.Select;
 
 public class ExampleTests {
 
@@ -17,9 +18,19 @@ public class ExampleTests {
         WebElement button=driver.findElement(By.xpath("//ul/li/a"));
         button.click();
         WebElement text = driver.findElement(By.xpath("//*[@id=\"content\"]/div/h3"));
-        Assert.assertEquals(text.getText(),"No Test");
+        Assert.assertEquals(text.getText(),"A/B Test Control");
 
+        driver.navigate().back();
+        /*Select drpList = new Select(driver.findElement(By.id("Dropdown List")));
+        drpList.selectByVisibleText("Option 1");*/
 
+        WebElement dropdownMenu = driver.findElement(By.xpath("//*[@id=\"content\"]/ul/li[11]/a"));
+        dropdownMenu.click();
 
+        WebElement dropOption = driver.findElement(By.xpath("//*[@id=\"dropdown\"]/option[2]"));
+        dropOption.click();
+
+        /*Select dropdown = new Select(dropdownMenu);
+        dropdown.selectByIndex(1);*/
     }
 }
