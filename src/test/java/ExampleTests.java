8990b9226da35e78e386154c069206d1f8768fe4
PavleBurJ <pavle.burjanadze@gmail.com>
2022-10-14 16:21:20 +0400

    - Navigate back to the main page - Click on DropDown - Select 'Option 1'

--- src/test/java/ExampleTests.java
+++ src/test/java/ExampleTests.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.List;
-
 public class ExampleTests {
 
     @Test
@@ -16,37 +14,9 @@ public class ExampleTests {
         WebDriver driver = new ChromeDriver();
         driver.get("http://the-internet.herokuapp.com/");
         driver.manage().window().maximize();
-        WebElement button=driver.findElement(By.xpath("//ul/li/a"));
+        WebElement button=driver.findElement(By.xpath("//*[@id=\"content\"]/ul/li[11]/a"));
         button.click();
-        WebElement text = driver.findElement(By.xpath("//*[@id=\"content\"]/div/h3"));
-        Assert.assertEquals(text.getText(),"No Test");
-    }
-    @Test
-    public void locatorTest(){
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://tutorialsninja.com/demo/index.php?route=product/category&path=33");
-        driver.manage().window().maximize();
-        //*[@id="content"]/div/div[2]/h1 - XPath
-        //#content > div > div.col-sm-4 > h1 - css
-        List<WebElement> anchors = driver.findElements(By.className("product-thumb"));
-        Assert.assertEquals(anchors.size(),2);
-         WebElement button=driver.findElement(By.partialLinkText("Canon EOS 5D"));
-        button.click();
-    }
-    @Test
-    public void browserCommands(){
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://tutorialsninja.com/demo/index.php?route=product/category&path=33");
-        // Get the title of the page
-        String title = driver.getTitle();
-        System.out.println("Title " + title);
-        // Get the current URL
-        String url = driver.getCurrentUrl();
-        System.out.println("URL " + url);
-        // Get the current page HTML source
-        String html = driver.getPageSource();
-        System.out.println("HTML " + html);
+        WebElement text = driver.findElement(By.xpath("//*[@id=\"dropdown\"]/option[2]"));
+        Assert.assertEquals(text.getText(),"Option 1");
     }
 }
